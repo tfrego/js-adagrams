905480be91041af8a33cd72fe9e7443531ff099c
Trang <trang.frego@gmail.com>
2018-11-14 16:16:40 -0800

    create scoreWord and pass tests

--- src/adagrams.js
+++ src/adagrams.js
@@ -44,6 +44,48 @@ const Adagrams = {
 
     return answer;
   },
+  scoreWord(word) {
+    const points = {
+      'A': 1,
+      'E': 1,
+      'I': 1,
+      'O': 1,
+      'U': 1,
+      'L': 1,
+      'N': 1,
+      'R': 1,
+      'S': 1,
+      'T': 1,
+      'D': 2,
+      'G': 2,
+      'B': 3,
+      'C': 3,
+      'M': 3,
+      'P': 3,
+      'F': 4,
+      'H': 4,
+      'V': 4,
+      'W': 4,
+      'Y': 4,
+      'K': 5,
+      'J': 8,
+      'X': 8,
+      'Q': 10,
+      'Z': 10
+    }
+    let score = 0;
+    const wordToScore = word.toUpperCase().split('');
+
+    wordToScore.forEach(function (letter) {
+      score += points[letter];
+    });
+
+    if (wordToScore.length >= 7 && wordToScore.length <= 10) {
+      score += 8;
+    }
+
+    return score;
+  },
 };
 
 // Do not remove this line or your tests will break!
